058ccd1bbb97e3bdc0d5374474d600f9574643ad
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2023-11-03 21:27:08 +0700

    Iterable dan Iterator

---
+++ src/programmer/zaman/now/collection/IterableApp.java
@@ -0,0 +1,24 @@
+package programmer.zaman.now.collection;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class IterableApp {
+    public static void main(String[] args) {
+        Iterable<String> names = List.of("Hanif", "Faiz", "hidayat");
+
+        for(var name : names){
+            System.out.println(name);
+        }
+
+
+        //MANUAL JADUL
+        System.out.println("ITERATOR");
+        Iterator<String> iterator = names.iterator();
+
+        while(iterator.hasNext()){
+            String name = iterator.next();
+            System.out.println(name);
+        }
+    }
+}
