18026e19b425da6cd83f593773e6760435a7d49c
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2024-03-04 23:11:57 +0700

    Konversi ke array

---
+++ src/programmer/zaman/now/collection/ArrayApp.java
@@ -0,0 +1,17 @@
+package programmer.zaman.now.collection;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayApp {
+    public static void main(String[] args) {
+
+        List<String> names = List.of("Hanif", "Faiz", "Hidayat");
+
+        Object[] objects = names.toArray();
+        String[] strings = names.toArray(new String[]{});
+
+        System.out.println(Arrays.toString(objects));
+        System.out.println(Arrays.toString(strings));
+    }
+}
